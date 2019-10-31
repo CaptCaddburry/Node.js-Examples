03fe2a6b304ff66c6d1f5c0f86750e5d658dd9a6
Jimmy Cadd <52796010+CaptCaddburry@users.noreply.github.com>
2019-10-31 13:56:10 -0400

    Update File Upload.js

--- File Upload Example/File Upload.js
+++ File Upload Example/File Upload.js
@@ -1,14 +1,14 @@
-import { createServer } from 'http'; //grants access to HTTP module
-import { IncomingForm } from 'formidable'; //formidable package used for uploading files to your server
-import { rename } from 'fs'; //grants access to the file system
+var http = require('http'); //grants access to HTTP module
+var formidable = require('formidable'); //formidable package used for uploading files to your server
+var fs = require('fs'); //grants access to the file system
 
-createServer(function (req, res) { //create a server(required, resolved)
+http.createServer(function (req, res) { //create a server(required, resolved)
   if (req.url == '/fileupload') {
-    var form = new IncomingForm(); //creates a new uploaded form
+    var form = new formidable.IncomingForm(); //creates a new uploaded form
     form.parse(req, function (err, fields, files) {
       var oldpath = files.filetoupload.path; //this path is for the temporary folder
       var newpath = 'C:/Users/Your Name/' + files.filetoupload.name;
-      rename(oldpath, newpath, function (err) { //saves the file to newpath
+      fs.rename(oldpath, newpath, function (err) { //saves the file to newpath
         if (err) throw err;
         res.write('File uploaded and moved!');
         res.end();
